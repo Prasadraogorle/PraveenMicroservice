9c7bf1b481b2f59633bbcc6d276228607ed7f673
Prasad rao <gorlep131@gmail.com>
2025-06-27 17:34:37 +0530

    all done

--- model.py
+++ model.py
@@ -29,7 +29,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # or restrict to your frontend domain
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
